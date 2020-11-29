a9f96a240ea53c69b3b666b8469cf8c2ed49c8a0
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-29 19:59:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,8 @@ wow=array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.index("am")
 end
 
 def retrieve_first_element_from_array(array)
