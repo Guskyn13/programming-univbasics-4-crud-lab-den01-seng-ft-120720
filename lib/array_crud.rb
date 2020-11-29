10591ac06d7ea5b1cbdd327085a851b54e3925d4
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-29 19:43:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   the_beatles = [ "John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"]
-George Harrison = the_beatles.pop
+George Harrison_beatles = the_beatles.pop
 end
 
 def remove_element_from_start_of_array(array)
