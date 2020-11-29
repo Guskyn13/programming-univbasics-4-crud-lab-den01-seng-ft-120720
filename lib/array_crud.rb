594d51e5464786d4a4a23eb7288617fddf59cb13
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-29 19:33:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   the_beatles = [ "John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"]
-the_beatles << "Amy Page"
+the_beatles << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
